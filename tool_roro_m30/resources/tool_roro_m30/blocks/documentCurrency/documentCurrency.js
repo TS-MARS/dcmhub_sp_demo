4548b096e5b19da4a65ba7a3b4478e1d38a165a4
XSA_BSCHMIT4 XSA_BSCHMIT4 <brigitte.schmitt@t-systems.com>
2023-01-25 13:57:52 +0000

    Rolls Royce new workspace

---
+++ tool_roro_m30/resources/tool_roro_m30/blocks/documentCurrency/documentCurrency.js
@@ -0,0 +1,21 @@
+sap.ui.define(["sap/ui/core/library", 'sap/uxap/BlockBase'], function (coreLibrary, BlockBase) {
+	"use strict";
+
+	var ViewType = coreLibrary.mvc.ViewType;
+
+	var DocumentCurrency = BlockBase.extend("tool_roro_m30.blocks.documentCurrency.DocumentCurrency", {
+		metadata: {
+			views: {
+				Collapsed: {
+					viewName: "tool_roro_m30.blocks.documentCurrency.DocumentCurrency",
+					type: ViewType.XML
+				},
+				Expanded: {
+					viewName: "tool_roro_m30.blocks.documentCurrency.DocumentCurrency",
+					type: ViewType.XML
+				}
+			}
+		}
+	});
+	return DocumentCurrency;
+}, true);
